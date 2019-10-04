770d7f96a931d098ab72e72fcd0de65466a35e06
Suneel <Suneel.Kanamarlapudi@cdk.com>
2019-10-04 12:52:16 +0530

    Initial commits with handcrafted package.json, webpack, app

---
+++ app/index.js
@@ -0,0 +1,18 @@
+/**
+ * Created by kanamars on 04/10/19.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class KVSShop extends React.Component{
+    render(){
+        return (
+            <React.Fragment>
+                Hurray React Initial Setup was successful
+            </React.Fragment>
+        );
+    }
+}
+
+ReactDOM.render(<KVSShop/>, document.getElementById("kvs-shop"));
